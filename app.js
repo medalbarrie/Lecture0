5870c6c6926f27180a6fb456944ea9fdf60bf9ec
medalbarrie <barrie_mohamedali@yahoo.co.uk>
2018-03-28 16:48:56 +0100

    logging Function to get result

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 var name = 'Medalbarrie';
 
 console.log(name);
+
+  function names(firstName, lastName) { 
+    var person = {
+     firstName:firstName,
+     lastName:lastName
+
+    }
+
+     return person;
+}
+
+console.log(names("Mohamed Ali", "Barrie"));
